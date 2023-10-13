e7b952dc91846cdede8519c0858f9330e0dd37ed
Sergei Artiukhin <serartinfo@gmail.com>
2023-10-13 17:07:36 +0200

    update

--- run.py
+++ run.py
@@ -4,17 +4,20 @@ import time
 import streamlit as st
 from tempfile import NamedTemporaryFile
 from audio_recorder_streamlit import audio_recorder
+import librosa
 
 
-@st.cache_resource()
-def load_model(model_name="base"):
-    return whisper.load_model(model_name, device="cpu")
+# @st.cache_resource()
+# def load_model(model_name="base"):
+#     return whisper.load_model(model_name, device="cpu")
 
 
 def transcribe(file_path, model_name="base"):
+    duration = librosa.get_duration(path=file_path)
+
     # Print results
-    st.text(f"File: {file_path}")
-    st.text(f"Model: {model_name}")
+    # st.text(f"File: {file_path}")
+    st.markdown(f"**Model: {model_name}.**")
 
     # Load the model
     st.text("Loading the model...")
@@ -43,39 +46,48 @@ def transcribe(file_path, model_name="base"):
     # End timing
     end_time = time.time()
 
+    # Time per duration
+    st.markdown(
+        f"**Time per duration: {(end_time - start_time) / duration * 60:.2f} seconds per minute!**"
+    )
+
     st.markdown(
         f"**Time taken for transcription: {end_time - start_time:.2f} seconds!**"
     )
+    st.markdown(f"**Audio duration: {duration:.2f} seconds.**")
+
     st.markdown(f"**Transcription: {result['text']}.**")
 
 
 def main():
     st.title("SigmaBank Whisper Support")
 
-    st.markdown(
-        """Press the button below to start recording, to stop recording just stop speaking. 
-        Wait untill the audio is uploaded before pressing the transcribe button."""
-    )
-
-    audio_bytes = audio_recorder()
-    if audio_bytes:
-        st.audio(audio_bytes, format="audio/wav")
-
-    # # File uploader for audio files
-    # uploaded_file = st.file_uploader(
-    #     "Choose an audio file", type=["mp3", "wav", "flac"]
+    # st.markdown(
+    #     """Press the button below to start recording, to stop recording just stop speaking.
+    #     Wait untill the audio is uploaded before pressing the transcribe button."""
     # )
 
+    # audio_bytes = audio_recorder()
+    # if audio_bytes:
+    #     st.audio(audio_bytes, format="audio/wav")
+
+    # File uploader for audio files
+    uploaded_file = st.file_uploader(
+        "Choose an audio file", type=["mp3", "wav", "flac"]
+    )
+
     # if uploaded_file:
-    #     st.audio(uploaded_file, format="audio/wav")  # Adjust the format accordingly
+    #     st.audio(
+    #         uploaded_file.read(), format="audio/wav"
+    #     )  # Adjust the format accordingly
 
     model = st.selectbox("Select a model", ["tiny", "base", "small"])
 
     # Button to transcribe the audio
     if st.button("Transcribe"):
-        if audio_bytes:
+        if uploaded_file:
             with NamedTemporaryFile(suffix=".wav", delete=False) as tmp_file:
-                tmp_file.write(audio_bytes)
+                tmp_file.write(uploaded_file.getvalue())
 
             transcribe(tmp_file.name, model)
         else:
